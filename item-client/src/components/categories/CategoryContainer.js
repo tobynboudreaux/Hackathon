b71b8fa9ce464d39a327f9b2b14aeed56be725f9
Toby Boudreaux <tobynboudreaux@gmail.com>
2021-02-11 11:58:35 -0600

    added categories directory

---
+++ item-client/src/components/categories/CategoryContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CategoryContainer = () => {
+    return (
+        <div>
+            <h1>Hello There</h1>
+            <h1>General Kenobi</h1>
+        </div>
+    )
+}
+
+export default CategoryContainer
